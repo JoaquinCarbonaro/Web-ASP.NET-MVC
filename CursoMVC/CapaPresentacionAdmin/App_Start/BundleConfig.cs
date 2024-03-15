a7da51456322e3c1f719a0ea7b28ec5a6bd70898
JoaquinCarbonaro <joacolcarbo@gmail.com>
2024-03-15 12:38:42 -0300

    finalizado

--- CursoMVC/CapaPresentacionAdmin/App_Start/BundleConfig.cs
+++ CursoMVC/CapaPresentacionAdmin/App_Start/BundleConfig.cs
@@ -13,6 +13,12 @@ namespace CapaPresentacionAdmin
 
             bundles.Add(new Bundle("~/bundles/complementos").Include(
                         "~/Scripts/fontawesome/all.min.js",
+                        "~/Scripts/DataTables/jquery.dataTables.js",
+                        "~/Scripts/DataTables/dataTables.responsive.js",
+                        "~/Scripts/loadingoverlay/loadingoverlay.min.js",
+                        "~/Scripts/sweetalert.min.js",
+                        "~/Scripts/jquery.validate.js",
+                        "~/Scripts/jquery-ui.js",
                         "~/Scripts/scripts.js"));
 
             //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -26,7 +32,13 @@ namespace CapaPresentacionAdmin
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.bundle.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/site.css",
+                "~/Content/DataTables/css/jquery.dataTables.css",
+                "~/Content/DataTables/css/responsive.dataTables.css",
+                "~/Content/sweetalert.css",
+                "~/Content/jquery-ui.css"
+                ));
         }
     }
 }
